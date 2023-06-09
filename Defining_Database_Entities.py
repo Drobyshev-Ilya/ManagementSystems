1e835d717e8ffe3bd61bc73ed221348f1505d210
Ilya <114656652+Drobyshev-Ilya@users.noreply.github.com>
2023-06-09 22:02:29 +0300

    Update Defining_Database_Entities.py

--- Defining_Database_Entities.py
+++ Defining_Database_Entities.py
@@ -12,61 +12,95 @@ session = Session()
 # Создание базовой модели
 Base = declarative_base()
 
-# Определение сущностей базы данных
+# Класс модели для данных студента
 class Student(Base):
     __tablename__ = 'students'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    surname = Column(String)
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    surname = Column(String(255), nullable=False)
     date_of_birth = Column(Date)
+    group_id = Column(Integer, ForeignKey('groups.id'))
 
-class Teacher(Base):
-    __tablename__ = 'teachers'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    surname = Column(String)
-    degree = Column(String)
+    group = relationship('Group', back_populates='students')
 
-class Course(Base):
-    __tablename__ = 'courses'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
-
+# Класс модели для данных оценки
 class Grade(Base):
     __tablename__ = 'grades'
-    id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True, index=True)
     student_id = Column(Integer, ForeignKey('students.id'))
     course_id = Column(Integer, ForeignKey('courses.id'))
     grade = Column(Float)
-    student = relationship("Student", backref="grades")
-    course = relationship("Course", backref="grades")
 
-    # Определение моделей данных для входных и выходных данных
+# Класс модели для данных преподавателя
+class Teacher(Base):
+    __tablename__ = 'teachers'
 
-class StudentCreate(BaseModel):
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    surname = Column(String(255), nullable=False)
+    degree = Column(String(255))
+
+# Класс модели для данных курса
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(255))
+
+# Класс модели для данных группы
+class Group(Base):
+    __tablename__ = 'groups'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    department_id = Column(Integer, ForeignKey('departments.id'))
+
+    department = relationship('Department', back_populates='groups')
+    students = relationship('Student', back_populates='group')
+
+# Класс модели для данных отделения
+class Department(Base):
+    __tablename__ = 'departments'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    groups = relationship('Group', back_populates='department')
+
+
+# Определение моделей данных для входных и выходных данных
+
+# Модель данных для создания нового студента
+class CreateStudent(BaseModel):
     name: str
     surname: str
     date_of_birth: date
+    group_id: int
 
-class StudentUpdate(BaseModel):
+
+# Модель данных для обновления информации о студенте
+class UpdateStudent(BaseModel):
     name: str
     surname: str
     date_of_birth: date
+    group_id: int
 
-class TeacherCreate(BaseModel):
-    name: str
-    surname: str
-    degree: str
-
-class CourseCreate(BaseModel):
+# Модель данных для создания нового курса
+class CreateCourse(BaseModel):
     name: str
     description: str
 
-class GradeCreate(BaseModel):
+
+# Модель данных для создания новой оценки
+class CreateGrade(BaseModel):
     student_id: int
     course_id: int
     grade: float
 
-class GradeUpdate(BaseModel):
+# Модель данных для обновления оценки
+class UpdateGrade(BaseModel):
+    student_id: int
+    course_id: int
     grade: float
